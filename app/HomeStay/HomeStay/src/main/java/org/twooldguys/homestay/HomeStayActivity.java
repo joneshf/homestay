bc126494db71ef1186272a3cf5cb2a46f0402740
joneshf <jones3.hardy@gmail.com>
2014-06-02 14:34:06 -0700

    Started with app.

---
+++ app/HomeStay/HomeStay/src/main/java/org/twooldguys/homestay/HomeStayActivity.java
@@ -0,0 +1,24 @@
+package org.twooldguys.homestay;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class HomeStayActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_stay);
+    }
+
+    public void gotoStudent(View view) {
+        Intent intent = new Intent(this, SearchActivity.class);
+        startActivity(intent);
+    }
+
+}
