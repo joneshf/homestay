3d2c319968ec0f783780a5d1d4a42e8e2a4ae371
joneshf <jones3.hardy@gmail.com>
2014-06-10 07:23:25 -0700

    Decided to dip down into java and just get it done.

---
+++ app/HomeStay/HomeStay/src/main/java/org/twooldguys/homestay/GetPrefs.java
@@ -0,0 +1,53 @@
+package org.twooldguys.homestay;
+
+import android.os.AsyncTask;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+
+/**
+ * Created by joneshf on 6/10/14.
+ */
+public class GetPrefs extends AsyncTask<Void, Void, String[]> {
+    private HostActivity act;
+
+    public GetPrefs(HostActivity act) {
+        this.act = act;
+    }
+
+    @Override
+    protected String[] doInBackground(Void... foo) {
+        ArrayList<String> list = new ArrayList<String>();
+        MongoClientURI uri = new MongoClientURI("mongodb://twooldguys:WowSoOld@ds041218.mongolab.com:41218/homestay");
+        MongoClient client = null;
+        try {
+            client = new MongoClient(uri);
+            DBCollection prefDB = client.getDB("homestay").getCollection("hostPreferences");
+            DBCursor cursor = prefDB.find();
+            for (DBObject obj : cursor.toArray()) {
+                System.out.println(obj);
+                list.add(obj.get("name").toString());
+            }
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return list.toArray(new String[list.size()]);
+    }
+
+    @Override
+    protected void onPostExecute(String[] result) {
+        ListView listView = (ListView) act.findViewById(R.id.preferences_list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(act, android.R.layout.simple_list_item_1);
+        listView.setAdapter(adapter);
+
+        adapter.addAll(result);
+    }
+}
--- app/HomeStay/HomeStay/src/main/scala/org/twooldguys/homestay/HostActivity.scala
+++ app/HomeStay/HomeStay/src/main/scala/org/twooldguys/homestay/HostActivity.scala
@@ -2,7 +2,7 @@ package org.twooldguys.homestay
 
 import android.content.{Intent, Context}
 import android.support.v7.app.ActionBarActivity
-import android.os.Bundle
+import android.os.{AsyncTask, Bundle}
 import android.view._
 import android.widget._
 
@@ -14,50 +14,15 @@ import com.mongodb.{BasicDBObject, DBCollection, MongoClient, MongoClientURI}
 import scala.util.{Failure, Success}
 import android.app.ListActivity
 
-class HostActivity extends ActionBarActivity {
 
-  // This is temporary.
-  val p = List(
-  ("Upstairs bedroom", new HostPreference(NonSmoking, YesPets, NoChild, Omnivore, Agnostic, Area)),
-  ("In-Law Suite", new HostPreference(EnjoysSmoking, YesPets, ThreePlusChild, Omnivore, Agnostic, Area))
-  )
+class HostActivity extends ActionBarActivity {
+  implicit def funToRunnable(fun: () => Unit) = new Runnable() { def run() = fun() }
 
   protected override def onCreate(savedInstanceState: Bundle) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_host)
 
-    val listView = findViewById(R.id.preferences_list).asInstanceOf[ListView]
-    val adapter = new ArrayAdapter[String](this, android.R.layout.simple_list_item_1)
-    listView.setAdapter(adapter)
-
-    val prefs = Future {
-      allPrefs
-    }
-    prefs onComplete {
-      case Success(ps) => {
-        // Get the intent.
-        //    val intent = getIntent
-        //    val prefs = intent.getStringArrayExtra("prefs")
-        // Grab the ListView
-        adapter.addAll(ps.toList)
-//        val adapter = new ArrayAdapter[String](this, android.R.layout.simple_list_item_1, ps)
-////        listView.setAdapter(adapter)
-      }
-      case Failure(err) => Toast.makeText(this, err.getMessage, Toast.LENGTH_SHORT)
-    }
-
-    //    // Should be able to swap this out with a db-backed collection and the rest just works.
-    ////    val f: Future[Array[String]] = Future {
-    //      val prefs = allPrefs
-    ////    }
-    ////    f onComplete {
-    ////      case Success(prefs) => {
-    //        for (pref <- prefs) {
-    //          Toast.makeText(this, pref, Toast.LENGTH_SHORT)
-    //        }
-////      }
-////      case Failure(reason) => Toast.makeText(this, reason.getMessage, Toast.LENGTH_LONG)
-////    }
+    new GetPrefs(this).execute()
   }
 
   override def onCreateOptionsMenu(menu: Menu): Boolean = {
@@ -73,15 +38,6 @@ class HostActivity extends ActionBarActivity {
     }
     case _ => super.onOptionsItemSelected(item)
   }
-
-  def allPrefs: Array[String] = {
-    val uri = new MongoClientURI("mongodb://twooldguys:WowSoOld@ds041218.mongolab.com:41218/homestay")
-    val client = new MongoClient(uri)
-    val prefDB = client.getDB("homestay").getCollection("hostPreferences")
-    val cursor = prefDB.find(new BasicDBObject, new BasicDBObject("name", 1))
-    cursor.toArray.map(_.get("name").toString).toArray
-  }
-  //  val allPrefs: Array[String] = p.map(_._1).toArray
 
   def addPreference =
     startActivity(new Intent(this, classOf[AddPreferenceActivity]))
